364961471069c5ea467ca6741497e8b8de172708
rodrigovaleretto <rodrigovaleretto@usp.br>
2020-05-28 08:36:13 -0300

    Comentários explicativos adicionados e melhoras nos laços

--- simplex.m
+++ simplex.m
@@ -1,17 +1,23 @@
-A = [1 1 1 0 0; 1 -1 0 1 0; -1 1 0 0 1]
-b = [6; 4; 4]
+% Para o programa funcionar corretamente inicialmente
+%é necessário definir as constantes do problema.
+% Esse prog resolve problemas de minimização, portanto
+%caso o problema seja de maximização, basta multiplicar
+%a func objetivo por -1.
+A = [1 1 1 0 0; -1 1 0 1 0; 1 0 0 0 1]
+b = [6; 2; 5]
 c = [-1; -2; 0; 0; 0]
 base = [3 4 5]
-maxit = 20
-it = 1
+maxit = 5
 x = zeros(maxit,length(c))
 
-while(it <= maxit)
+%Inicio do código
+for it = [1:maxit]
   B = A(:, base)
   xB = inv(B)*b
   lambdat = c(base)' * inv(B)
   ctil = c' - lambdat*A
   
+  %Laço que plota todos os resultados de x
   for i = [1:length(c)]
     for j = [1:length(base)]
       if(i == base(j))
@@ -45,5 +51,4 @@ while(it <= maxit)
     endif
   endfor
   base(l) = k
-  it++
-endwhile
+endfor
